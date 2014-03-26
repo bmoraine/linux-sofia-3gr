072ba9984108c2dc9ceb4ceb58916ee89e6dc633
Xiao He <xiao.he@intel.com>
2014-03-26 20:59:50 +0800

    devmem: [REVERTME] Get rid of security check on devmem to ungate VPU
    [Xavier Roumegue]
    	Let's use this quick hack for now but must be revised later

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -738,7 +738,7 @@ static loff_t memory_lseek(struct file *file, loff_t offset, int orig)
 #if defined(CONFIG_DEVMEM) || defined(CONFIG_DEVKMEM) || defined(CONFIG_DEVPORT)
 static int open_port(struct inode *inode, struct file *filp)
 {
-	return capable(CAP_SYS_RAWIO) ? 0 : -EPERM;
+	return 0; // capable(CAP_SYS_RAWIO) ? 0 : -EPERM;
 }
 #endif
 
