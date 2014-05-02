74e2ed185fc5351ef8c3368967baa77d86743383
Sander Eikelenboom <linux@eikelenboom.it>
2014-05-02 15:09:27 +0200

    ALSA: usb-audio: Prevent printk ratelimiting from spamming kernel log while DEBUG not defined
    This (widely used) construction:
    
    if(printk_ratelimit())
    	dev_dbg()
    
    Causes the ratelimiting to spam the kernel log with the "callbacks suppressed"
    message below, even while the dev_dbg it is supposed to rate limit wouldn't
    print anything because DEBUG is not defined for this device.
    
    [  533.803964] retire_playback_urb: 852 callbacks suppressed
    [  538.807930] retire_playback_urb: 852 callbacks suppressed
    [  543.811897] retire_playback_urb: 852 callbacks suppressed
    [  548.815745] retire_playback_urb: 852 callbacks suppressed
    [  553.819826] retire_playback_urb: 852 callbacks suppressed
    
    So use dev_dbg_ratelimited() instead of this construction.
    
    Signed-off-by: Sander Eikelenboom <linux@eikelenboom.it>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Change-Id: Ib177fe89bb905ab768eee9c442a23e8f9c9c02c0
    Reviewed-on: https://opticm6.rds.intel.com/r/82777
    Tested-by: TCloud Jenkins
    Reviewed-by: Gregory Herrero <gregory.herrero@intel.com>

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -1501,8 +1501,7 @@ static void retire_playback_urb(struct snd_usb_substream *subs,
 	 * The error should be lower than 2ms since the estimate relies
 	 * on two reads of a counter updated every ms.
 	 */
-	if (printk_ratelimit() &&
-	    abs(est_delay - subs->last_delay) * 1000 > runtime->rate * 2)
+	if (abs(est_delay - subs->last_delay) * 1000 > runtime->rate * 2)
 		dev_dbg_ratelimited(&subs->dev->dev,
 			"delay: estimated %d, actual %d\n",
 			est_delay, subs->last_delay);
