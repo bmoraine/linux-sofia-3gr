5b304db0665b753c4d2185c03a6978ef07e53b67
Xavier Roumegue <xavier.roumegue@intel.com>
2014-06-17 16:31:58 +0200

    x86, apic: SoFIA APIC driver
    Access registers through hypercall
    
    Add send_IPI_all callback.
    
    This adds suspend/resume core callbacks to shutdown
    the local apic timer since there is PM operations enabled
    in apic.c because of no_lapic_pm early parameter
    
    Signed-off-by: Xavier Roumegue <xavier.roumegue@intel.com>
    
    x86, apic, sofia: Enable LOCAL_VECTOR vector to IPI send
    
    Signed-off-by: Xavier Roumegue <xavier.roumegue@intel.com>

---
+++ arch/x86/kernel/apic/apic_sofia.c
@@ -0,0 +1,259 @@
+/*
+ * Copyright (C) 2014 Intel Mobile Communications GmbH
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/smp.h>
+#include <linux/percpu.h>
+#include <linux/cpumask.h>
+#include <linux/syscore_ops.h>
+#include <asm/apic.h>
+#include <asm/irq_vectors.h>
+
+#include <sofia/vmm_guest_api.h>
+#include <sofia/vmm_platform_service.h>
+
+#define LAPIC_PADDR_BASE   0xFEE00000
+
+static u32 sofia_apic_mem_read(u32 reg);
+static void sofia_apic_mem_write(u32 reg, u32 v);
+
+static void sofia_apic_send_ipi_mask(const struct cpumask *mask, int vector)
+{
+	if ((vector != RESCHEDULE_VECTOR) &&
+		(vector != CALL_FUNCTION_SINGLE_VECTOR) &&
+		(vector != CALL_FUNCTION_VECTOR) &&
+		(vector != LOCAL_TIMER_VECTOR)) {
+		pr_err("%s: Unknown vector %x\n", __func__, vector);
+		BUG();
+	}
+
+	vmm_ipi_post(vector, cpumask_bits(mask)[0]);
+}
+
+
+static void sofia_send_ipi_allbutself(int vector)
+{
+	unsigned int vcpus;
+
+	if (!(num_online_cpus() > 1))
+		return;
+
+	vcpus = (1 << num_possible_cpus()) - 1;
+	vcpus &= ~(1 << smp_processor_id());
+	vmm_ipi_post(vector, vcpus);
+}
+
+static void sofia_send_IPI_all(int vector)
+{
+	unsigned int vcpus;
+
+	vcpus = (1 << num_possible_cpus()) - 1;
+	vmm_ipi_post(vector, vcpus);
+
+}
+
+static int sofia_wakeup_secondary_cpu(int apicid, unsigned long start_ip)
+{
+	/* FIXME: Not so good to highjack the parameter.. */
+	unsigned long hack_start_ip = (unsigned long)__pa(startup_32_smp);
+	vmm_start_vcpu(apicid, hack_start_ip);
+	return 0;
+}
+
+static int sofia_cpu_present_to_apicid(int cpu)
+{
+	return cpu;
+}
+
+static int sofia_check_phys_apicid_present(int apicid)
+{
+	return true;
+}
+
+static int sofia_apic_id_valid(int apicid)
+{
+	return true;
+}
+
+static int sofia_apic_suspend(void)
+{
+	unsigned v;
+
+	v = sofia_apic_mem_read(APIC_LVTT);
+	v |= (APIC_LVT_MASKED | LOCAL_TIMER_VECTOR);
+	sofia_apic_mem_write(APIC_LVTT, v);
+	sofia_apic_mem_write(APIC_TMICT, 0);
+
+	return 0;
+}
+
+static void sofia_apic_resume(void)
+{
+	unsigned v;
+
+	v = sofia_apic_mem_read(APIC_LVTT);
+	v &= ~(APIC_LVT_MASKED);
+	v |= LOCAL_TIMER_VECTOR;
+	sofia_apic_mem_write(APIC_LVTT, v);
+
+}
+
+static struct syscore_ops sofia_apic_syscore_ops = {
+	.resume		= sofia_apic_resume,
+	.suspend	= sofia_apic_suspend,
+};
+
+static int sofia_apic_probe(void)
+{
+	register_syscore_ops(&sofia_apic_syscore_ops);
+
+	return 1;
+}
+
+static unsigned sofia_default_get_apic_id(unsigned long x)
+{
+	unsigned apic_id = vmm_vcpu_id();
+
+	return apic_id;
+}
+
+static u32 sofia_apic_mem_read(u32 reg)
+{
+	unsigned addr = LAPIC_PADDR_BASE + reg;
+	unsigned mask = 0xFFFFFFFF;
+	unsigned val;
+	unsigned ret;
+
+	ret = vmm_reg_read(addr, &val, mask);
+	if (ret) {
+		pr_err("%s: Read %x APIC register failed\n",
+				__func__, addr);
+		pr_err("%s: Continue with native read...\n", __func__);
+
+		return native_apic_mem_read(reg);
+
+	}
+
+	return val;
+}
+
+
+static void sofia_apic_mem_write(u32 reg, u32 v)
+{
+	unsigned addr = LAPIC_PADDR_BASE + reg;
+	unsigned mask = 0xFFFFFFFF;
+	int ret;
+
+	switch (reg) {
+	case APIC_LVTT:
+	case APIC_TMICT:
+	case APIC_TDCR:
+	case APIC_TMCCT:
+		ret = vmm_reg_write(addr, v, mask);
+		if (ret) {
+			pr_err("%s: Write %x APIC register failed\n",
+					__func__, addr);
+			pr_err("%s: Continue with native write...\n", __func__);
+			native_apic_mem_write(reg, v);
+		}
+		break;
+	default:
+		return;
+	}
+}
+
+static int sofia_early_logical_apicid(int cpu)
+{
+	return BIT(cpu);
+}
+
+static void sofia_apic_eoi_write(u32 reg, u32 value)
+{
+	vmm_virq_eoi(value);
+}
+
+static int sofia_apic_id_registered(void)
+{
+	return physid_isset(read_apic_id(), phys_cpu_present_map);
+}
+
+
+static void sofia_init_lapic_ldr(void) { }
+static void sofia_apic_wait_icr_idle(void) { }
+static void sofia_apic_ipi_self(int vector) { }
+
+static struct apic apic_sofia = {
+
+	.name				= "sofia",
+	.probe				= sofia_apic_probe,
+	.acpi_madt_oem_check		= NULL,
+	.apic_id_valid			= sofia_apic_id_valid,
+	.apic_id_registered		= sofia_apic_id_registered,
+
+	.irq_delivery_mode		= 0,
+	.irq_dest_mode			= 1,
+
+	.target_cpus			= NULL,
+	.disable_esr			= 0,
+	.dest_logical			= 0,
+	.check_apicid_used		= NULL,
+	.check_apicid_present		= NULL,
+
+	.vector_allocation_domain	= NULL,
+	.init_apic_ldr			= sofia_init_lapic_ldr,
+
+	.ioapic_phys_id_map		= NULL,
+	.setup_apic_routing		= NULL,
+	.multi_timer_check		= NULL,
+	.cpu_present_to_apicid		= sofia_cpu_present_to_apicid,
+	.apicid_to_cpu_present		= NULL,
+	.setup_portio_remap		= NULL,
+	.check_phys_apicid_present	= sofia_check_phys_apicid_present,
+	.enable_apic_mode		= NULL,
+	.phys_pkg_id			= NULL,
+	.mps_oem_check			= NULL,
+
+	.get_apic_id			= sofia_default_get_apic_id,
+	.set_apic_id			= NULL,
+	.apic_id_mask			= 0x0F << 24,
+
+	.cpu_mask_to_apicid_and		= NULL,
+
+	.send_IPI_mask			= sofia_apic_send_ipi_mask,
+	.send_IPI_mask_allbutself	= NULL,
+	.send_IPI_allbutself		= sofia_send_ipi_allbutself,
+	.send_IPI_all			= sofia_send_IPI_all,
+	.send_IPI_self			= sofia_apic_ipi_self,
+
+	.wakeup_secondary_cpu		= sofia_wakeup_secondary_cpu,
+
+	.trampoline_phys_low		= DEFAULT_TRAMPOLINE_PHYS_LOW,
+	.trampoline_phys_high		= DEFAULT_TRAMPOLINE_PHYS_HIGH,
+
+	.wait_for_init_deassert		= NULL,
+
+	.smp_callin_clear_local_apic	= NULL,
+	.inquire_remote_apic		= NULL,
+
+	.read				= sofia_apic_mem_read,
+	.write				= sofia_apic_mem_write,
+	.eoi_write			= sofia_apic_eoi_write,
+	.icr_read			= NULL,
+	.icr_write			= NULL,
+	.wait_icr_idle			= sofia_apic_wait_icr_idle,
+	.safe_wait_icr_idle		= NULL,
+
+	.x86_32_early_logical_apicid	= sofia_early_logical_apicid,
+};
+
+apic_driver(apic_sofia);
+
