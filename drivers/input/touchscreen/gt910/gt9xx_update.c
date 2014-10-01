facea75a3ee938e2fe23a08c5a6408ca89efb638
Antoine Bouyer <antoine.bouyer@intel.com>
2014-10-01 11:42:19 +0200

    input, touchscreen, gt910: fix 64b warnings
    Signed-off-by: Antoine Bouyer <antoine.bouyer@intel.com>

--- drivers/input/touchscreen/gt910/gt9xx_update.c
+++ drivers/input/touchscreen/gt910/gt9xx_update.c
@@ -829,7 +829,7 @@ static u8 gup_check_update_file(struct i2c_client *client,
 
 	got_file_flag = 0x00;
 	if (path) {
-		GTP_DEBUG("Update File path:%s, %d", path, strlen(path));
+		GTP_DEBUG("Update File path:%s, %zu", path, strlen(path));
 		update_msg.file = filp_open(path, O_RDONLY, 0);
 
 		if (IS_ERR(update_msg.file)) {
