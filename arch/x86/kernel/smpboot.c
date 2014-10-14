a45318d9a405fa84fbbcef46262f25725496457b
Jian Sun <jian.sun@intel.com>
2014-10-14 11:53:10 +0200

    x86, smpboot, sofia: Update sofia hypercalls according to new naming convention
    Signed-off-by: Xavier Roumegue <xavier.roumegue@intel.com>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -1464,7 +1464,7 @@ static inline void hlt_play_dead(void)
 }
 
 #ifdef CONFIG_X86_INTEL_SOFIA
-extern void vmm_stop_vcpu(unsigned int vcpu);
+extern void mv_stop_vcpu(unsigned int vcpu);
 #endif
 
 void native_play_dead(void)
@@ -1472,7 +1472,7 @@ void native_play_dead(void)
 	play_dead_common();
 
 #ifdef CONFIG_X86_INTEL_SOFIA
-	vmm_stop_vcpu(smp_processor_id());
+	mv_stop_vcpu(smp_processor_id());
 #endif
 	tboot_shutdown(TB_SHUTDOWN_WFS);
 
