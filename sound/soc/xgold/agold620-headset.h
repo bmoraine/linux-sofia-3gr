ff1f9590c21fafcee4c0f66fbd0fa47c51cd66cc
Hans Holmberg <hans.holmberg@intel.com>
2014-11-11 08:59:12 +0100

    sound: soc: add jack detection to xgold machine driver
    Adds jack detection and headset button handling utilizing
    the asoc framework for reporting.
    
    Detection mechanism moved in from the the xgold headset switch
    driver.
    
    Issue: GMINL-3567
    Change-Id: I8fd22ab58f961b038ab534f061e640d64bd61514
    Signed-off-by: Hans Holmberg <hans.holmberg@intel.com>

--- sound/soc/xgold/agold620-headset.h
+++ sound/soc/xgold/agold620-headset.h
@@ -15,8 +15,10 @@
  *
  */
 
+#include <linux/input.h>
+
 /* FIXME */
-#include <../../sound/soc/codecs/agold_acc_det.h>
+#include <../../codecs/agold_acc_det.h>
 
 #define AHJ_TYPE_MIN_MV 475
 #define AHJ_TYPE_MAX_MV 1700
@@ -24,10 +26,18 @@
 #define HEADPHONE_MIN_MV 0
 #define HEADPHONE_MAX_MV 50
 
-static struct hook_key_cfg xgold_hs_keymap[] = {
-	{0, 50, KEY_FORWARDMAIL , 0},
-	{100, 150, KEY_VOLUMEUP, 0},
-	{275, 325, KEY_VOLUMEDOWN, 0},
+struct hs_key_cfg {
+	int min_mv;
+	int max_mv;
+	enum snd_jack_types type;
+	int key_code;
+	int pressed;
+};
+
+static struct hs_key_cfg xgold_hs_keymap[] = {
+	{0, 50, SND_JACK_BTN_0 , KEY_MEDIA, 0},
+	{100, 150, SND_JACK_BTN_1, KEY_VOLUMEUP, 0},
+	{275, 325, SND_JACK_BTN_2, KEY_VOLUMEDOWN, 0},
 };
 
 #define XGOLD_DETECT_INSERTION        0x80FB
@@ -37,30 +47,26 @@ static struct hook_key_cfg xgold_hs_keymap[] = {
 #define XGOLD_OFF_EDG1                3
 #define XGOLD_OFF_EDG2                11
 
-#define ACC_VBIAS_ENABLE \
-{\
-	struct agold_afe_acc_det acc_det_par;\
-	acc_det_par.vumic_conf.vmode = AGOLD_AFE_VUMIC_MODE_ULP;\
-	acc_det_par.vumic_conf.hzmic = AGOLD_AFE_HZVUMIC_NORMAL_POWER_DOWN;\
-	acc_det_par.vumic_conf.vmicsel = AGOLD_AFE_VMICSEL_2_1_V;\
-	acc_det_par.micldo_mode = AGOLD_AFE_MICLDO_MODE_NORMAL;\
-	acc_det_par.xb_mode = AGOLD_AFE_XB_ON;\
-	if (agold_afe_set_acc_det_with_lock(acc_det_par)) { \
-		dev_err(headset_device_data->sdev.dev, \
-				"ACC_VBIAS_ENABLE ERROR\n"); \
-	} \
-}
+#define VBIAS_SETTLING_TIME_MS	20
 
-#define ACC_VBIAS_ULP_ON \
-{\
-	struct agold_afe_acc_det acc_det_par;\
-	acc_det_par.vumic_conf.vmode = AGOLD_AFE_VUMIC_MODE_ULP;\
-	acc_det_par.vumic_conf.hzmic = AGOLD_AFE_HZVUMIC_NORMAL_POWER_DOWN;\
-	acc_det_par.vumic_conf.vmicsel = AGOLD_AFE_VMICSEL_1_9_V;\
-	acc_det_par.micldo_mode = AGOLD_AFE_MICLDO_MODE_LOW_POWER;\
-	acc_det_par.xb_mode = AGOLD_AFE_XB_OFF;\
-	if (agold_afe_set_acc_det_with_lock(acc_det_par)) { \
-		dev_err(headset_device_data->sdev.dev, \
-				"ACC_VBIAS_ULP_ON ERROR\n"); \
-	} \
-}
+/**
+ * Different VBIAS settings
+**/
+enum xgold_vbias {
+	XGOLD_VBIAS_ENABLE,
+	XGOLD_VBIAS_ULP_ON,
+};
+
+enum xgold_headset_type {
+	XGOLD_HEADSET_REMOVED,
+	XGOLD_HEAD_SET,
+	XGOLD_HEAD_PHONE,
+	XGOLD_INVALID,
+	XGOLD_ERROR
+};
+
+
+
+
+
+
--- sound/soc/xgold/xgold_machine.c
+++ sound/soc/xgold/xgold_machine.c
@@ -24,10 +24,19 @@
 #include <linux/platform_device.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/of.h>
+#include <linux/io.h>
+#include <linux/delay.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
+#include <sound/jack.h>
+#include <linux/iio/iio.h>
+#include <linux/iio/machine.h>
+#include <linux/iio/consumer.h>
+#include <linux/iio/driver.h>
+
+#include "agold620-headset.h"
 
 #define PCL_LOCK_EXCLUSIVE	1
 #define PCL_OPER_ACTIVATE	0
@@ -41,6 +50,16 @@
 		pr_debug("snd: mac: "fmt, ##arg)
 
 int audio_native_mode;
+
+struct xgold_mc_private {
+	struct iio_channel *iio_client;
+	unsigned int jack_irq;
+	unsigned int button_irq;
+	void __iomem *mmio_base;
+	int buttons_enabled;
+};
+
+static struct snd_soc_jack hs_jack;
 
 /* XGOLD machine DAPM */
 static const struct snd_soc_dapm_widget xgold_dapm_widgets[] = {
@@ -70,9 +89,10 @@ static const struct snd_soc_dapm_route audio_map[] = {
 
 static int xgold_snd_init(struct snd_soc_pcm_runtime *runtime)
 {
-
 	struct snd_soc_codec *codec = runtime->codec;
 	int ret = 0;
+	int i;
+	int type;
 
 	xgold_debug("%s\n", __func__);
 
@@ -95,6 +115,35 @@ static int xgold_snd_init(struct snd_soc_pcm_runtime *runtime)
 	snd_soc_dapm_enable_pin(&codec->dapm, "Handset Mic");
 
 	ret = snd_soc_dapm_sync(&codec->dapm);
+	if (ret) {
+		xgold_err("Failed to enable dapm pins\n");
+		return ret;
+	}
+
+	/* Set up jack detection */
+
+	type = SND_JACK_HEADSET;
+	for (i = 0; i < ARRAY_SIZE(xgold_hs_keymap); i++)
+		type |= xgold_hs_keymap[i].type;
+
+	ret = snd_soc_jack_new(codec, "XGOLD Audio Jack",
+			type,
+			&hs_jack);
+
+	if (ret) {
+		xgold_err("Jack creation failed\n");
+		return ret;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(xgold_hs_keymap); i++) {
+		ret = snd_jack_set_key(hs_jack.jack,
+			xgold_hs_keymap[i].type,
+			xgold_hs_keymap[i].key_code);
+		if (ret) {
+			xgold_err("Failed to set headset key\n");
+			return ret;
+		}
+	}
 
 	return ret;
 }
@@ -176,11 +225,257 @@ static struct snd_soc_card xgold_snd_card = {
 	.num_links = ARRAY_SIZE(xgold_dai),
 };
 
+/* Call to AFE to change the VBIAS settings */
+static void configure_vbias(enum xgold_vbias state)
+{
+	struct agold_afe_acc_det acc_det_par;
+
+	switch (state) {
+	case XGOLD_VBIAS_ENABLE:
+		acc_det_par.vumic_conf.vmode = AGOLD_AFE_VUMIC_MODE_ULP;
+		acc_det_par.vumic_conf.hzmic =
+			AGOLD_AFE_HZVUMIC_NORMAL_POWER_DOWN;
+		acc_det_par.vumic_conf.vmicsel = AGOLD_AFE_VMICSEL_2_1_V;
+		acc_det_par.micldo_mode = AGOLD_AFE_MICLDO_MODE_NORMAL;
+		acc_det_par.xb_mode = AGOLD_AFE_XB_ON;
+		break;
+	case XGOLD_VBIAS_ULP_ON:
+		acc_det_par.vumic_conf.vmode = AGOLD_AFE_VUMIC_MODE_ULP;
+		acc_det_par.vumic_conf.hzmic =
+			AGOLD_AFE_HZVUMIC_NORMAL_POWER_DOWN;
+		acc_det_par.vumic_conf.vmicsel = AGOLD_AFE_VMICSEL_1_9_V;
+		acc_det_par.micldo_mode = AGOLD_AFE_MICLDO_MODE_LOW_POWER;
+		acc_det_par.xb_mode = AGOLD_AFE_XB_OFF;
+		break;
+	default:
+		return;
+	}
+
+	if (agold_afe_set_acc_det_with_lock(acc_det_par))
+		xgold_err("Error when setting VBIAS!\n");
+
+}
+
+static u32 read_state(struct xgold_mc_private *mc_drv_ctx)
+{
+	int volt, ret;
+
+	ret = iio_read_channel_raw(mc_drv_ctx->iio_client, &volt);
+	if (ret < 0) {
+		xgold_err("Unable to read channel volt\n");
+		return XGOLD_ERROR;
+	}
+
+	if (volt >= AHJ_TYPE_MIN_MV &&
+			volt <= AHJ_TYPE_MAX_MV)
+			return XGOLD_HEAD_SET;
+		else if (volt >= HEADPHONE_MIN_MV &&
+				volt <= HEADPHONE_MAX_MV)
+			return XGOLD_HEAD_PHONE;
+		else if (volt > AHJ_TYPE_MAX_MV)
+			return XGOLD_HEADSET_REMOVED;
+		else
+			return XGOLD_INVALID;
+}
+
+static void xgold_jack_check(struct xgold_mc_private *mc_drv_ctx)
+{
+	u32 state, old_state;
+	u32 detect;
+	int status = 0;
+	enum xgold_vbias vbias;
+
+	configure_vbias(XGOLD_VBIAS_ENABLE);
+
+	/* First, make sure we have a stable state.
+	   Headset insertion takes a bit of time(~> 500ms),
+	   so make sure that two consecutive reads agree.
+	*/
+	do {
+		msleep(250);
+		old_state = read_state(mc_drv_ctx);
+		msleep(250);
+		state = read_state(mc_drv_ctx);
+	} while (state != old_state);
+
+	if (XGOLD_ERROR == state) {
+		xgold_err("Unable to determine state.\n");
+		return;
+	}
+
+	switch (state) {
+	case XGOLD_HEAD_PHONE:
+		xgold_debug("Headphone inserted\n");
+		vbias = XGOLD_VBIAS_ULP_ON;
+		detect = XGOLD_DETECT_REMOVAL;
+		status = SND_JACK_HEADPHONE;
+		mc_drv_ctx->buttons_enabled = false;
+		break;
+	case XGOLD_HEAD_SET:
+		xgold_debug("Headset inserted\n");
+		vbias = XGOLD_VBIAS_ENABLE;
+		detect = XGOLD_DETECT_REMOVAL_HOOK;
+		status = SND_JACK_HEADSET;
+		mc_drv_ctx->buttons_enabled = true;
+		break;
+	case XGOLD_HEADSET_REMOVED:
+		xgold_debug("Headphone/headset removed\n");
+		vbias = XGOLD_VBIAS_ULP_ON;
+		detect = XGOLD_DETECT_INSERTION;
+		mc_drv_ctx->buttons_enabled = false;
+		break;
+	default:
+		xgold_debug("Invalid headset state!\n");
+		return;
+	}
+
+	configure_vbias(vbias);
+	msleep(VBIAS_SETTLING_TIME_MS);
+
+	/* Check if there really is a state change */
+	if (status != (hs_jack.status & SND_JACK_HEADSET)) {
+		iowrite32(detect, mc_drv_ctx->mmio_base);
+		snd_soc_jack_report(&hs_jack, status, SND_JACK_HEADSET);
+	}
+}
+
+static irqreturn_t xgold_jack_detection(int irq, void *data)
+{
+	xgold_jack_check((struct xgold_mc_private *)data);
+	return IRQ_HANDLED;
+}
+
+static void xgold_button_check(struct xgold_mc_private *mc_drv_ctx)
+{
+	int ret, volt, i;
+	int key_index = -1;
+	u32 detect;
+	int status;
+	enum snd_jack_types type;
+
+	ret = iio_read_channel_raw(mc_drv_ctx->iio_client, &volt);
+	if (ret < 0) {
+		xgold_err("Unable to read channel volt\n");
+		return;
+	}
+
+	/* check if a key has been pressed and remember this*/
+	for (i = 0; i < ARRAY_SIZE(xgold_hs_keymap); i++) {
+		if ((volt >= xgold_hs_keymap[i].min_mv) &&
+			(volt <= xgold_hs_keymap[i].max_mv)) {
+			xgold_hs_keymap[i].pressed = 1;
+			key_index = i;
+			break;
+		}
+	}
+
+	if (key_index > -1) {
+		xgold_debug("button press index %d\n", key_index);
+		type = xgold_hs_keymap[key_index].type;
+		status = type;
+		detect = XGOLD_DETECT_HOOK_RELEASE;
+	} else {
+		/* key released, figure out which */
+		for (i = 0; i < ARRAY_SIZE(xgold_hs_keymap); i++) {
+			if (1 == xgold_hs_keymap[i].pressed) {
+				xgold_debug("button release, index %d\n", i);
+				xgold_hs_keymap[i].pressed = 0;
+				key_index = i;
+				type = xgold_hs_keymap[key_index].type;
+				status = 0;
+			}
+		}
+		detect = XGOLD_DETECT_REMOVAL_HOOK;
+	}
+
+	if (key_index > -1) {
+		snd_soc_jack_report(&hs_jack, status, type);
+		iowrite32(detect, mc_drv_ctx->mmio_base);
+	}
+}
+
+static irqreturn_t xgold_button_detection(int irq, void *data)
+{
+	struct xgold_mc_private *mc_drv_ctx = data;
+	if (mc_drv_ctx->buttons_enabled)
+		xgold_button_check(mc_drv_ctx);
+	return IRQ_HANDLED;
+}
+
+static int xgold_jack_probe(struct platform_device *pdev,
+		struct xgold_mc_private *mc_drv_ctx)
+{
+	struct resource *res;
+	int ret;
+
+	mc_drv_ctx->buttons_enabled = 0;
+
+	res = platform_get_resource_byname(pdev,
+			IORESOURCE_MEM, "headset-registers");
+	if (res == NULL) {
+		xgold_err("no I/O memory defined in platform data\n");
+		return -EINVAL;
+	}
+
+	mc_drv_ctx->mmio_base = ioremap(res->start, resource_size(res));
+
+	xgold_debug("ioremap %p\n", mc_drv_ctx->mmio_base);
+
+	if (mc_drv_ctx->mmio_base == NULL) {
+		xgold_err("failed to remap I/O memory\n");
+		return -ENOMEM;
+	}
+
+	mc_drv_ctx->jack_irq =
+			platform_get_irq_byname(pdev, "acd1");
+	if (mc_drv_ctx->jack_irq < 0) {
+		xgold_err("no headset plug irq defined\n");
+		return -EINVAL;
+	}
+
+	mc_drv_ctx->iio_client = iio_channel_get(NULL, "ACCID_SENSOR");
+	if (IS_ERR(mc_drv_ctx->iio_client)) {
+		xgold_err("iio channel error\n");
+		return -EINVAL;
+	}
+
+	/* Configure the Accessory settings to detect Insertion */
+	iowrite32(XGOLD_DETECT_INSERTION, mc_drv_ctx->mmio_base);
+
+	ret = devm_request_threaded_irq(&(pdev->dev), mc_drv_ctx->jack_irq,
+			NULL,
+			xgold_jack_detection,
+			IRQF_SHARED | IRQF_ONESHOT, "jack_irq", mc_drv_ctx);
+	if (ret) {
+		xgold_err("setup of jackirq failed!\n");
+		return -EINVAL;
+	}
+
+	mc_drv_ctx->button_irq =
+			platform_get_irq_byname(pdev, "acd2");
+	if (mc_drv_ctx->button_irq < 0) {
+		xgold_err("no irq_hook irq defined\n");
+		return -EINVAL;
+	}
+
+	ret = devm_request_threaded_irq(&(pdev->dev), mc_drv_ctx->button_irq,
+			NULL,
+			xgold_button_detection,
+			IRQF_SHARED | IRQF_ONESHOT, "button_irq", mc_drv_ctx);
+	if (ret < 0) {
+		xgold_err("setup of button irq failed!\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static int xgold_mc_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
 	struct device_node *codec_of_node = NULL;
 	struct snd_soc_dai_link *dai_link;
+	struct xgold_mc_private *mc_drv_ctx;
 	const char *codec_dai_name = NULL;
 	int ret = 0;
 	int i;
@@ -303,16 +598,36 @@ static int xgold_mc_probe(struct platform_device *pdev)
 #endif
 
 	ret = snd_soc_register_card(&xgold_snd_card);
-
-	if (ret < 0)
+	if (ret < 0) {
 		xgold_err("%s: unable to register sound card err %d\n",
 			  __func__, ret);
+		return ret;
+	}
 
-	return ret;
+	mc_drv_ctx = devm_kzalloc(&pdev->dev, sizeof(*mc_drv_ctx), GFP_ATOMIC);
+	if (!mc_drv_ctx) {
+		xgold_err("Allocation failed!\n");
+		return -ENOMEM;
+	}
+
+	platform_set_drvdata(pdev, mc_drv_ctx);
+
+	ret = xgold_jack_probe(pdev, mc_drv_ctx);
+	if (ret < 0) {
+		xgold_err("Jack detection probe failed.\n");
+		/* Allow machine probe to succeed anyway */
+	}
+
+	return 0;
 }
 
 static int xgold_mc_remove(struct platform_device *pdev)
 {
+	struct xgold_mc_private *mc_drv_ctx = platform_get_drvdata(pdev);
+
+	if (mc_drv_ctx && mc_drv_ctx->iio_client)
+		iio_channel_release(mc_drv_ctx->iio_client);
+
 	xgold_debug("%s:\n", __func__);
 	return 0;
 }
