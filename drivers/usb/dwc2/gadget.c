a34ee7ceb346743d92644071175ad7fdf6a4bf42
Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
2014-11-28 10:17:00 +0100

    usb: dwc2: gadget: kill requests after disabling ep
    kill_all_requests() can flush the fifo. Call it after disabling the
    endpoint. Moreover, remove even the current IN request so that next
    IN request after s3c_hsotg_ep_enable can be properly handled.
    
    Signed-off-by: Mian Yousaf Kaukab <yousaf.kaukab@intel.com>

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -2612,8 +2612,6 @@ static int s3c_hsotg_ep_disable(struct usb_ep *ep)
 	epctrl_reg = dir_in ? DIEPCTL(index) : DOEPCTL(index);
 
 	spin_lock_irqsave(&hsotg->lock, flags);
-	/* terminate all requests with shutdown */
-	kill_all_requests(hsotg, hs_ep, -ESHUTDOWN, false);
 
 	hsotg->fifo_map &= ~(1<<hs_ep->fifo_index);
 	hs_ep->fifo_index = 0;
@@ -2629,6 +2627,9 @@ static int s3c_hsotg_ep_disable(struct usb_ep *ep)
 
 	/* disable endpoint interrupts */
 	s3c_hsotg_ctrl_epint(hsotg, hs_ep->index, hs_ep->dir_in, 0);
+
+	/* terminate all requests with shutdown */
+	kill_all_requests(hsotg, hs_ep, -ESHUTDOWN, true);
 
 	spin_unlock_irqrestore(&hsotg->lock, flags);
 	return 0;
