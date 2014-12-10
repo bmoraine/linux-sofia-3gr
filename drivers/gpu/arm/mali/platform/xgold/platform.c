2254353f8a13312bcd481c3f1383c933e948842e
Tobias Selig <tobias.selig@intel.com>
2014-12-10 16:54:05 +0100

    gpu, mali, utgard: Cleanup platform device initialization
    Signed-off-by: Tobias Selig <tobias.selig@intel.com>
    Change-Id: If167fa36fbe1f6fe19533462271598ae8551b0fa
    Reviewed-on: https://gerrit.rds.intel.com/r/49522
    Tested-by: Jenkins T-Cloud
    Reviewed-by: Claudio Koehl <claudio.koehl@intel.com>
    Reviewed-by: Fabrizio Iannetti <fabrizio.iannetti@intel.com>

--- drivers/gpu/arm/mali/platform/xgold/platform.c
+++ drivers/gpu/arm/mali/platform/xgold/platform.c
@@ -341,24 +341,30 @@ int mali_platform_device_init(struct platform_device *pdev)
 		return -1;
 	}
 
-	/* Use AMR DVFS with 500ms intervall */
-	plf_data.gpu_data->control_interval = 500;
-	plf_data.gpu_data->set_freq = mali_gpu_set_clock_step;
-	plf_data.gpu_data->get_clock_info = mali_report_gpu_clock_info;
-	plf_data.gpu_data->get_freq = mali_gpu_get_clock_step;
-
-	mali_dev_pm.dvfs_wq = alloc_ordered_workqueue("mali_setting_clock_wq",
-		WQ_NON_REENTRANT);
-	if (mali_dev_pm.dvfs_wq == 0) {
-		mali_err("Unable to create workqueue for dvfs\n");
-		return -1;
-	}
-
-	/* Runtime resume after probe will switch things on */
+#if defined(CONFIG_PM_RUNTIME)
+	/* Runtime resume will switch things on */
 	mali_dev_pm.curr_pm_state = MALI_PLF_PM_STATE_D3;
 	mali_dev_pm.resume_pm_state = GPU_INITIAL_PM_STATE;
-	mali_dev_pm.req_clock_index = mali_dev_pm.resume_pm_state - 1;
 
+	/* Need to set GPU QoS on bootup */
+	restore_gpu_qos = true;
+#else
+	mali_dev_pm.curr_pm_state = GPU_MAX_PM_STATE;
+	mali_dev_pm.resume_pm_state = GPU_MAX_PM_STATE;
+
+	ret = platform_device_pm_set_state(pdev,
+		mali_dev_pm.pm_states[mali_dev_pm.curr_pm_state]);
+	if (ret < 0) {
+		mali_err("Device pm set state failed (%d)\n", ret);
+		return ret;
+	}
+
+	/* Need to set GPU QoS on bootup */
+	xgold_noc_qos_set("GPU");
+	mali_dbg("Set GPU QoS\n");
+#endif
+
+	mali_dev_pm.req_clock_index = mali_dev_pm.resume_pm_state - 1;
 	mali_dev_pm.pdev = pdev;
 
 	ret = mali_platform_memory_layout(plf_data.gpu_data);
@@ -381,15 +387,26 @@ int mali_platform_device_init(struct platform_device *pdev)
 		return ret;
 	}
 
+	/* Use AMR DVFS with 500ms intervall */
+	plf_data.gpu_data->control_interval = 500;
+	plf_data.gpu_data->set_freq = mali_gpu_set_clock_step;
+	plf_data.gpu_data->get_clock_info = mali_report_gpu_clock_info;
+	plf_data.gpu_data->get_freq = mali_gpu_get_clock_step;
+
+	mali_dev_pm.dvfs_wq = alloc_ordered_workqueue("mali_setting_clock_wq",
+		WQ_NON_REENTRANT);
+	if (mali_dev_pm.dvfs_wq == 0) {
+		mali_err("Unable to create workqueue for dvfs\n");
+		return -1;
+	}
+
 	ret = platform_device_add_data(pdev, &xgold_mali_gpu_data,
 		sizeof(xgold_mali_gpu_data));
 	if (ret) {
 		mali_err("Device platform_device_add_data\n");
+		destroy_workqueue(mali_dev_pm.dvfs_wq);
 		return ret;
 	}
-
-	/* Need to set GPU QoS on bootup */
-	restore_gpu_qos = true;
 
 #if defined(CONFIG_PM_RUNTIME)
 	pm_runtime_set_autosuspend_delay(&(pdev->dev), 1000);
