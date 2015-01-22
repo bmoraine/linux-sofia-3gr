6cc0a4d99161f2e3440b04ed920a06094055e521
Ian Chang <ian.chang@intel.com>
2015-01-22 09:54:45 +0800

    x86, dts, sofia_3gr: spcu thermal dts enabling for 3gr/3gr_garnet
    seperate 3gr/3gr_garnet hirq dts from 3g one due to
    different thermal dts numbers and usage
    
    register 5 spcu thermal devices at kernel thermal class,
    which are dts[0,3] for cpu zone and dts4 for gpu zone
    
    dts[0,3] are mapped to /sys/class/thermal/thermal_zone[0,3]
    dts4 is mapped to /sys/class/thermal/thermal_zone4
    
    spcu config/ctrl/stat registers for dts are correct'd.
    
    Change-Id: I319157edd6bc30b85ea22d73103b52a2742f38e1
    UTP-Id: SMS06351725
    Signed-off-by: Ian Chang <ian.chang@intel.com>
    Reviewed-on: https://opticm6.rds.intel.com/r/56767
    Tested-by: TCloud Jenkins
    Reviewed-by: Cooper Chiou <cooper.chiou@intel.com>
    Reviewed-by: Xavier Roumegue <xavier.roumegue@intel.com>

--- arch/x86/boot/dts/xgold/sofia_3gr_soc/irq-hirq.h
+++ arch/x86/boot/dts/xgold/sofia_3gr_soc/irq-hirq.h
@@ -1,1 +1,35 @@
-#include "../sofia_3g_soc/irq-hirq.h"
+/*
+ * Copyright (C) 2014 Intel Mobile Communications GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _HIRQ_IRQ_H
+#define _HIRQ_IRQ_H
+
+#define HIRQ_OFFSET 512
+
+/* HIRQ nodes from HIRQ domain point of view */
+#define HIRQ_VMM_BROADCAST		(512 - HIRQ_OFFSET)
+#define HIRQ_PMIC_TEST			(513 - HIRQ_OFFSET)
+#define HIRQ_HIRQ_TEST			(514 - HIRQ_OFFSET)
+#define HIRQ_PRH_SIGNALING		(515 - HIRQ_OFFSET)
+#define HIRQ_VTIMER				(516 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM0_LOW	(517 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM0_HIGH	(518 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM1_LOW	(519 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM1_HIGH	(520 - HIRQ_OFFSET)
+#define HIRQ_RTC_ALARM			(521 - HIRQ_OFFSET)
+#define HIRQ_CPU_CLK_NOTIFY		(522 - HIRQ_OFFSET)
+#define HIRQ_MODEM_DVFS			(523 - HIRQ_OFFSET)
+#define HIRQ_VM_VERIFY			(524 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM2_LOW	(525 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM2_HIGH	(526 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM3_LOW	(527 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM3_HIGH	(528 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM4_LOW	(529 - HIRQ_OFFSET)
+#define HIRQ_SPCU_THERM4_HIGH	(530 - HIRQ_OFFSET)
+
+#endif /* _HIRQ_IRQ_H */
