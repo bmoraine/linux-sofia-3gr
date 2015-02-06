dd08f9e51a631623db33e2913c179cc7f897268b
Gregory Herrero <gregory.herrero@intel.com>
2015-02-06 12:18:12 +0100

    usb: dwc2: host: stop all urbs on disconnect
    This prevent any urb submission while there is no device
    connected.
    
    This also prevent this error:
    hub 1-0:1.0: port 1 disabled by hub (EMI?), re-enabling...
    
    Change-Id: Ie8cc459d51b80e38c691086ad6472dc262539bad
    Signed-off-by: Gregory Herrero <gregory.herrero@intel.com>

--- drivers/usb/dwc2/core_intr.c
+++ drivers/usb/dwc2/core_intr.c
@@ -383,6 +383,8 @@ static void dwc2_handle_disconnect_intr(struct dwc2_hsotg *hsotg)
 		dwc2_is_host_mode(hsotg) ? "Host" : "Device",
 		dwc2_op_state_str(hsotg));
 
+	dwc2_hcd_disconnect(hsotg);
+
 	/* Change to L3 (OFF) state */
 	hsotg->lx_state = DWC2_L3;
 
