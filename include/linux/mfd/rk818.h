e48d605a652836e1362754b7b1be40bb380a5d9a
ArnoldX Lu <arnoldx.lu@intel.com>
2015-02-15 10:34:42 +0800

    kernel, rtc: add copyright for RK818 header file.
    Add copyright for RK818 header file.
    
    Change-Id: I298cd846c1d193b0c69d033fd5608973baa42899
    UTP-Id: SMS06496243
    Signed-off-by: ArnoldX Lu <arnoldx.lu@intel.com>
    Reviewed-on: https://opticm6.rds.intel.com/r/64514
    Tested-by: TCloud Jenkins
    Reviewed-by: Sheng Wang <sheng.wang@intel.com>
    Reviewed-by: Hendry Yang <hendry.yang@intel.com>
    Reviewed-by: Xavier Roumegue <xavier.roumegue@intel.com>

--- include/linux/mfd/rk818.h
+++ include/linux/mfd/rk818.h
@@ -1,13 +1,15 @@
-/* include/linux/regulator/rk818.h
+/*
+ * Copyright (c) 2014, Fuzhou Rockchip Electronics Co., Ltd
+ * Author: zhangqing <zhangqing@rock-chips.com>
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
  */
 #ifndef __LINUX_REGULATOR_rk818_H
