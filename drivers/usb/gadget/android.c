2faf37c255bd87f7f901b4c7bc561e69641b72d3
Gregory Herrero <gregory.herrero@intel.com>
2015-02-19 13:01:13 +0100

    Revert "android: remove inappropriate S_IRUGO on device attributes"
    This reverts commit db3f2c31eb169adcc9b5b6b84545c1d67732f58b.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1181,7 +1181,7 @@ static ssize_t dvcdfx_reset_store(struct device *pdev,
 	return size;
 }
 
-static DEVICE_ATTR(reset_dfx, S_IWUSR, NULL, dvcdfx_reset_store);
+static DEVICE_ATTR(reset_dfx, S_IRUGO | S_IWUSR, NULL, dvcdfx_reset_store);
 
 static ssize_t dvcdfx_enable_store(struct device *pdev,
 		  struct device_attribute *attr, const char *buf, size_t size)
@@ -1216,7 +1216,7 @@ static ssize_t dvcdfx_enable_store(struct device *pdev,
 	return -EINVAL;
 }
 
-static DEVICE_ATTR(enable_dfx, S_IWUSR, NULL, dvcdfx_enable_store);
+static DEVICE_ATTR(enable_dfx, S_IRUGO | S_IWUSR, NULL, dvcdfx_enable_store);
 
 static ssize_t dvcdfx_bFunctionProtocol_show(struct device *dev,
 					     struct device_attribute *attr,
@@ -1334,7 +1334,7 @@ static ssize_t dvctrace_reset_store(struct device *pdev,
 	return size;
 }
 
-static DEVICE_ATTR(reset_trace, S_IWUSR, NULL, dvctrace_reset_store);
+static DEVICE_ATTR(reset_trace, S_IRUGO | S_IWUSR, NULL, dvctrace_reset_store);
 
 static ssize_t dvctrace_enable_store(struct device *pdev,
 		  struct device_attribute *attr, const char *buf, size_t size)
@@ -1369,7 +1369,8 @@ static ssize_t dvctrace_enable_store(struct device *pdev,
 	return -EINVAL;
 }
 
-static DEVICE_ATTR(enable_trace, S_IWUSR, NULL, dvctrace_enable_store);
+static DEVICE_ATTR(enable_trace, S_IRUGO | S_IWUSR, NULL,
+		   dvctrace_enable_store);
 
 static ssize_t dvctrace_bFunctionProtocol_show(struct device *dev,
 					       struct device_attribute *attr,
