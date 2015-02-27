5013263eb82965f2828b3c9cd2b2c39ce5f0d8dc
Antoine Bouyer <antoine.bouyer@intel.com>
2015-02-27 10:42:54 +0100

    ASoC: pmic afe: fix for accessory detection
     - Cleanup register mask
     - Correct acc detection settings
     - Request power before writing afe registers
    
    Signed-off-by: Antoine Bouyer <antoine.bouyer@intel.com>
    Signed-off-by: Thomas Joergensen <thomas.joergensen@intel.com>
    Change-Id: I3a4fdfbbd7ab9fabe51fa58238b2219a82d0dc02
    UTP-Id: SMS06566031

--- sound/soc/codecs/intel_pmic_acc_det.c
+++ sound/soc/codecs/intel_pmic_acc_det.c
@@ -32,11 +32,11 @@
 
 /* Masks for AFE Register AUDIO IN CTRL fileds */
 
-/* Bit 5-6 of AFE_AUDIOINCTRL2_REG */
+/* Bit 5-6 of AFE_AUDIOINCTRL3_REG */
 #define AFE_MASK_AUDIOINCTRL_VUMIC_MODE   0x9F
 
-/* Bit 3 of AFE_AUDIOINCTRL3_REG*/
-#define AFE_MASK_AUDIOINCTRL_HZ_VUMIC     0xF7
+/* Bit 3 of AFE_AUDIOINCTRL4_REG*/
+#define AFE_MASK_AUDIOINCTRL_HZ_VUMIC     0xFD
 
 /* Bit 7 of AFE_AUDIOINCTRL3_REG */
 #define AFE_MASK_AUDIOINCTRL_VMIC_SELL     0x7F
@@ -45,21 +45,25 @@
 #define AFE_MASK_AUDIOINCTRL_VMIC_SELH     0xFE
 
 /* Bit 4-5 of AFE_AUDIOINCTRL3_REG */
-#define AFE_MASK_AUDIOINCTRL_MIC_LDO      0xCF
+#define AFE_MASK_AUDIOINCTRL_MIC_LDO      0xE7
 
-/* Bit 4 of AFE_AUDIOINCTRL2_REG */
+/* Bit 4 of AFE_AUDIOINCTRL3_REG */
 #define AFE_MASK_AUDIOINCTRL_VMIC_MODE    0xEF
 
 /* Bit 7 of AFE_BCON4_REG */
 #define AFE_MASK_BCON_XB_ON               0x7F
 
-/* Bit Shifts for AGOLD AFE Register AUDIO IN CTRL fields */
-#define AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE   5
-#define AFE_SHIFT_AUDIOINCTRL_HZ_VUMIC     3
+/* Bit Shifts for PMIC AFE Register AUDIOINCTRL3 fields */
 #define AFE_SHIFT_AUDIOINCTRL_VMIC_SELL    7
-#define AFE_SHIFT_AUDIOINCTRL_VMIC_SELH    0
-#define AFE_SHIFT_AUDIOINCTRL_MIC_LDO      4
+#define AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE   5
 #define AFE_SHIFT_AUDIOINCTRL_VMIC_MODE    4
+
+/* Bit Shifts for PMIC AFE Register AUDIOINCTRL4 fields */
+#define AFE_SHIFT_AUDIOINCTRL_MIC_LDO      3
+#define AFE_SHIFT_AUDIOINCTRL_HZ_VUMIC     2
+#define AFE_SHIFT_AUDIOINCTRL_VMIC_SELH    0
+
+/* Bit Shifts for PMIC AFE Register BCON4 fields */
 #define AFE_SHIFT_BCON_XB_ON               7
 
 
@@ -80,7 +84,7 @@ struct afe_accessory_detect pmic_acc_det_data = {
 		.vumic_conf = {
 			.vmode = AFE_VUMIC_MODE_ULP,
 			.hzmic = AFE_HZVUMIC_NORMAL_POWER_DOWN,
-			.vmicsel = AFE_VMICSEL_2_2_V,
+			.vmicsel = AFE_VMICSEL_2_1_V,
 		},
 		.micldo_mode = AFE_MICLDO_MODE_LOW_POWER,
 		.xb_mode = AFE_XB_ON,
@@ -147,131 +151,117 @@ int pmic_afe_set_acc_det_with_lock(struct afe_acc_det acc_det_par)
 }
 EXPORT_SYMBOL_GPL(pmic_afe_set_acc_det_with_lock);
 
-/*Function calculates the register values for AGOLD_AFE_AUDIOINCTRL and
- *AGOLD_AFE_BCON.Certain register bits are requested both by MIC use cases
- *(like speech call, recording) and accessory identification use cases.
- *This function arrives at a consolidated values by combining 2 sets of values.
- *Prioriy will be given to MIC/headset use case settings.
- *When the MIC is not being used, accessory identification values requested
- *will be used. afe_private_data->codec->mutex has to be acquired before
- *calling this function.
+/* Function calculates the register values for AFE_AUDIOINCTRLi_REG and
+ * AFE_BCON. Certain register bits are requested both by MIC use cases
+ * (like speech call, recording) and accessory identification use cases.
+ * This function arrives at a consolidated values by combining 2 sets of values.
+ * Prioriy will be given to MIC/headset use case settings.
+ * When the MIC is not being used, accessory identification values requested
+ * will be used. afe_private_data->codec->mutex has to be acquired before
+ * calling this function.
  */
 int pmic_afe_calculate_acc_settings(unsigned int reg,
 		unsigned int requested_value,
 		unsigned int *final_value)
 {
-	int result = -EINVAL;
-	if (NULL != final_value) {
-		*final_value = requested_value;
-		/* The bits for accessory identification are spread over
-		AFE_AUDIOINCTRL2_REG, AFE_AUDIOINCTRL3_REG and AFE_BCON4_REG
-		registers */
-		if (AFE_AUDIOINCTRL3_REG == reg) {
-			/* Clear the bits that are unused by ASOC/DAPM,
-			 * so that they are freshly calculated */
-			*final_value &= ~(1 <<
-				AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE);
-			*final_value = (*final_value &
-				AFE_MASK_AUDIOINCTRL_VUMIC_MODE);
-			if ((0 == pmic_acc_det_data.mic_ldo_on)) {
-				*final_value |=
+	if (!final_value)
+		return -EINVAL;
+
+	*final_value = requested_value;
+	/* The bits for accessory identification are spread over
+	AFE_AUDIOINCTRL2_REG, AFE_AUDIOINCTRL3_REG and AFE_BCON4_REG
+	registers */
+	if (AFE_AUDIOINCTRL3_REG == reg) {
+		/* Clear the bits that are unused by ASOC/DAPM,
+		 * so that they are freshly calculated */
+		*final_value &= AFE_MASK_AUDIOINCTRL_VUMIC_MODE;
+
+		if (AFE_VUMIC_MODE_POWER_DOWN == pmic_acc_det_data.mic_ldo_on) {
+			*final_value |=
 				(pmic_acc_det_data.acc_det_param.
-				vumic_conf.vmode  <<
-				AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE);
+				 vumic_conf.vmode & 0x3) <<
+				AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE;
 
 			/* Changing MICLDO mode to low power while vmic
 			 * mode is active results in false interrupt.
 			 * Change mic ldo and vmic mode together.
 			 * As request is to turn off ldo from ASOC,
 			 * vmic mode can be put off */
-				*final_value = (*final_value &
-					AFE_MASK_AUDIOINCTRL_VMIC_MODE);
-				*final_value |= (0 <<
-				AFE_SHIFT_AUDIOINCTRL_VMIC_MODE);
-				*final_value = (*final_value &
-						AFE_MASK_AUDIOINCTRL_VMIC_SELL);
-				*final_value |=
-					((pmic_acc_det_data.acc_det_param.
-					vumic_conf.vmicsel & 0x1) <<
-					AFE_SHIFT_AUDIOINCTRL_VMIC_SELL);
-			} else {
-				/* DAPM sequence has mic ldo enabled.
-				Program vmic, vumic and mic ldo together*/
-				pr_debug("%s MIC LDO is On. Using ASOC values\n"
-					, __func__);
-				/* if any of mic is active, vumicmode
-				should not be off */
-				*final_value = (*final_value &
-					AFE_MASK_AUDIOINCTRL_VUMIC_MODE);
-				*final_value |=
-					(AFE_VUMIC_MODE_NORMAL <<
-					AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE);
+			*final_value &= AFE_MASK_AUDIOINCTRL_VMIC_MODE;
+			*final_value |= AFE_VUMIC_MODE_POWER_DOWN <<
+				AFE_SHIFT_AUDIOINCTRL_VMIC_MODE;
 
-				/* ASOC request for vmic mode bias widget
-				is stored and programmed here while enabling
-				mic ldo */
-				*final_value = (*final_value &
-						AFE_MASK_AUDIOINCTRL_VMIC_MODE);
-				*final_value |=
-					(pmic_acc_det_data.vmic_mode <<
-					AFE_SHIFT_AUDIOINCTRL_VMIC_MODE);
-			}
-		} else if (AFE_AUDIOINCTRL4_REG == reg) {
-			/* Clear the bits that are unused by ASOC/DAPM,
-			so that they are freshly calculated */
-			*final_value &= ~(1 << AFE_SHIFT_AUDIOINCTRL_MIC_LDO);
-
-			if ((0 == pmic_acc_det_data.mic_ldo_on)) {
-				/* DAPM sequence has mic ldo disabled
-				use acc settings */
-				pr_debug("%s MIC LDO is off. Using acc value\n",
-						__func__);
-				*final_value = (*final_value &
-						AFE_MASK_AUDIOINCTRL_MIC_LDO);
-				*final_value |=
-					(pmic_acc_det_data.acc_det_param.
-					micldo_mode <<
-						AFE_SHIFT_AUDIOINCTRL_MIC_LDO);
-
-				*final_value = (*final_value &
-						AFE_MASK_AUDIOINCTRL_VMIC_SELH);
-				*final_value |=
-					(((pmic_acc_det_data.acc_det_param.
-					vumic_conf.vmicsel & 0x2) >> 1) <<
-					AFE_SHIFT_AUDIOINCTRL_VMIC_SELH);
-			} else {
-				/* DAPM sequence has mic ldo enabled.
-				Program vmic, vumic and mic ldo together*/
-				pr_debug("%s MIC LDO is On. Using ASOC values\n"
-					, __func__);
-				/* Update mic ldo and vmic mode together */
-				*final_value = *final_value &
-						AFE_MASK_AUDIOINCTRL_MIC_LDO;
-				*final_value |= (AFE_MICLDO_MODE_NORMAL <<
-						AFE_SHIFT_AUDIOINCTRL_MIC_LDO);
-			}
-			*final_value = *final_value &
-					AFE_MASK_AUDIOINCTRL_HZ_VUMIC;
+			*final_value &= AFE_MASK_AUDIOINCTRL_VMIC_SELL;
 			*final_value |=
 				(pmic_acc_det_data.acc_det_param.
-						vumic_conf.hzmic <<
-					AFE_SHIFT_AUDIOINCTRL_HZ_VUMIC);
-		} else if (AFE_BCON4_REG == reg) {
-			if (0 == (*final_value & ~(AFE_MASK_BCON_XB_ON))) {
-				pr_debug("%s Using ACC requested val for XB\n",
-							__func__);
-				/* Request for XB off. So use val requested by
-				ACC driver */
-				*final_value |=
-					(pmic_acc_det_data.
-						acc_det_param.xb_mode <<
-						AFE_SHIFT_BCON_XB_ON);
-			}
+				 vumic_conf.vmicsel & 0x1) <<
+				AFE_SHIFT_AUDIOINCTRL_VMIC_SELL;
+		} else {
+			/* DAPM sequence has mic ldo enabled.
+			Program vmic, vumic and mic ldo together*/
+			pr_debug("%s MIC LDO is On. Using ASOC values\n",
+				       __func__);
+			/* if any of mic is active, vumicmode
+			should not be off */
+			*final_value &= AFE_MASK_AUDIOINCTRL_VUMIC_MODE;
+			*final_value |= AFE_VUMIC_MODE_NORMAL <<
+				AFE_SHIFT_AUDIOINCTRL_VUMIC_MODE;
+
+			/* ASOC request for vmic mode bias widget
+			is stored and programmed here while enabling
+			mic ldo */
+			*final_value &= AFE_MASK_AUDIOINCTRL_VMIC_MODE;
+			*final_value |=
+				(pmic_acc_det_data.vmic_mode & 0x1) <<
+				AFE_SHIFT_AUDIOINCTRL_VMIC_MODE;
 		}
-		result = 0;
+	} else if (AFE_AUDIOINCTRL4_REG == reg) {
+		/* Clear the bits that are unused by ASOC/DAPM,
+		so that they are freshly calculated */
+		*final_value &= AFE_MASK_AUDIOINCTRL_MIC_LDO;
+
+		if (AFE_VUMIC_MODE_POWER_DOWN == pmic_acc_det_data.mic_ldo_on) {
+			/* DAPM sequence has mic ldo disabled
+			use acc settings */
+			pr_debug("%s MIC LDO is off. Using acc value\n",
+					__func__);
+			*final_value |=
+				(pmic_acc_det_data.acc_det_param.
+				 micldo_mode & 0x3) <<
+				AFE_SHIFT_AUDIOINCTRL_MIC_LDO;
+
+			*final_value &= AFE_MASK_AUDIOINCTRL_VMIC_SELH;
+			*final_value |=
+				((pmic_acc_det_data.acc_det_param.
+				vumic_conf.vmicsel & 0x2) >> 1) <<
+				AFE_SHIFT_AUDIOINCTRL_VMIC_SELH;
+		} else {
+			/* DAPM sequence has mic ldo enabled.
+			Program vmic, vumic and mic ldo together*/
+			pr_debug("%s MIC LDO is On. Using ASOC values\n"
+				, __func__);
+			/* Update mic ldo and vmic mode together */
+			*final_value |= AFE_MICLDO_MODE_NORMAL <<
+				AFE_SHIFT_AUDIOINCTRL_MIC_LDO;
+		}
+		*final_value &= AFE_MASK_AUDIOINCTRL_HZ_VUMIC;
+		*final_value |=
+			(pmic_acc_det_data.acc_det_param.
+			 vumic_conf.hzmic & 0x1) <<
+			AFE_SHIFT_AUDIOINCTRL_HZ_VUMIC;
+	} else if (AFE_BCON4_REG == reg) {
+		if (0 == (*final_value & ~(AFE_MASK_BCON_XB_ON))) {
+			pr_debug("%s Using ACC requested val for XB\n",
+						__func__);
+			/* Request for XB off. So use val requested by
+			ACC driver */
+			*final_value |=
+				(pmic_acc_det_data.acc_det_param.
+				 xb_mode & 0x1) << AFE_SHIFT_BCON_XB_ON;
+		}
 	}
 
-	return result;
+	return 0;
 }
 
 /* Activity on MIC. Update the mic_ldo status */
