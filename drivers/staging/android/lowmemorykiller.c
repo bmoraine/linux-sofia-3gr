51260c7d2016267613e98ad3380bc0ddec14f0a6
Stephon Fan <stephon.fan@intel.com>
2015-05-06 13:58:17 +0800

    lowmemorykiller: Consider isolated cma pages
    Don't consider the CMA free pages as real kernel free-able memory,
    so that the LMK threshold can be adjusted to lower values
    
    Change-Id: I8c439f71bf91a7222c133e3d0df9148d220bc256
    Signed-off-by: Stephon Fan <stephon.fan@intel.com>
    Signed-off-by: Stanley Lee <stanley.lee@intel.com>
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Original-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-8028
    Reviewed-on: https://android.intel.com:443/434777
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14933
    Reviewed-on: https://android.intel.com:443/454951

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -89,9 +89,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
-	int cma_free = global_page_state(NR_FREE_CMA_PAGES);
+	int cma_free = global_page_state(NR_FREE_CMA_PAGES)
+				+ global_page_state(NR_FREE_CMA_ISOLATE_PAGES);
 
-	if (other_free > cma_free)
 		other_free -= cma_free;
 
 	if (lowmem_adj_size < array_size)
