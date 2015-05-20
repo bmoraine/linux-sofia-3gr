f3bd62140cbf06d132fb61f849f416f1e535cdda
Gregory Herrero <gregory.herrero@intel.com>
2015-05-20 11:40:22 +0200

    usb: dwc2: host: resume only if bus is suspended
    Port can be resumed in bus_resume callback.
    In this case, there is no need to drive resume a second time
    when hcd ask for it.
    
    Signed-off-by: Gregory Herrero <gregory.herrero@intel.com>
    
    Change-Id: I486ff39187d2c84016689aeda195bba6d6f5f821
    UTP-Id: SMS07221604
    Reviewed-on: https://opticm6.rds.intel.com/r/98108
    Tested-by: TCloud Jenkins
    Reviewed-by: Xavier Roumegue <xavier.roumegue@intel.com>
    Reviewed-by: Ashraf Mohammed <ashraf.mohammed@intel.com>
    Reviewed-by: UTP Service

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -1566,7 +1566,8 @@ static int dwc2_hcd_hub_control(struct dwc2_hsotg *hsotg, u16 typereq,
 			writel(0, hsotg->regs + PCGCTL);
 			usleep_range(20000, 40000);
 
-			dwc2_port_resume(hsotg);
+			if (hsotg->bus_suspended)
+				dwc2_port_resume(hsotg);
 			break;
 
 		case USB_PORT_FEAT_POWER:
