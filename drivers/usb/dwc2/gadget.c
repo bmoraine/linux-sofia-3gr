43e1eb1f80f3054c8bd7099d69466202135b26f4
Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
2015-06-17 15:39:23 +0200

    usb: dwc2: gadget: handle reset interrupt before endpoint interrupts
    If system is loaded, reset, enum-done and setup interrupt can occur
    at the same time. In this case, current interrupt handling sequence
    will handle setup packet's interrupt before handling reset interrupt.
    Which will break the enumeration process. Correct sequence would be
    to handle reset, enum done and then any other endpoint interrupts.
    
    Signed-off-by: Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
    
    Change-Id: I2f2ce3f1943c2391e8da397af42eaee561caaa10
    UTP-Id: SMS07580559
    Reviewed-on: https://opticm6.rds.intel.com/r/111389
    Tested-by: TCloud <tcloud_tdd@intel.com>
    Reviewed-by: Yousaf Kaukab <yousaf.kaukab@intel.com>
    Reviewed-by: UTP Service

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -2463,6 +2463,36 @@ irq_retry:
 
 	gintsts &= gintmsk;
 
+	if (gintsts & GINTSTS_RESETDET) {
+		dev_dbg(hsotg->dev, "%s: USBRstDet\n", __func__);
+
+		writel(GINTSTS_RESETDET, hsotg->regs + GINTSTS);
+
+		/* This event must be used only if controller is suspended */
+		if (hsotg->lx_state == DWC2_L2) {
+			dwc2_exit_hibernation(hsotg, true);
+			hsotg->lx_state = DWC2_L0;
+		}
+	}
+
+	if (gintsts & (GINTSTS_USBRST | GINTSTS_RESETDET)) {
+
+		u32 usb_status = readl(hsotg->regs + GOTGCTL);
+		u32 connected = hsotg->connected;
+
+		dev_dbg(hsotg->dev, "%s: USBRst\n", __func__);
+		dev_dbg(hsotg->dev, "GNPTXSTS=%08x\n",
+			readl(hsotg->regs + GNPTXSTS));
+
+		writel(GINTSTS_USBRST, hsotg->regs + GINTSTS);
+
+		/* Report disconnection if it is not already done. */
+		s3c_hsotg_disconnect(hsotg);
+
+		if (usb_status & GOTGCTL_BSESVLD && connected)
+			s3c_hsotg_core_init_disconnected(hsotg, true);
+	}
+
 	if (gintsts & GINTSTS_ENUMDONE) {
 		writel(GINTSTS_ENUMDONE, hsotg->regs + GINTSTS);
 
@@ -2492,36 +2522,6 @@ irq_retry:
 			if (daint_in & 1)
 				s3c_hsotg_epint(hsotg, ep, 1);
 		}
-	}
-
-	if (gintsts & GINTSTS_RESETDET) {
-		dev_dbg(hsotg->dev, "%s: USBRstDet\n", __func__);
-
-		writel(GINTSTS_RESETDET, hsotg->regs + GINTSTS);
-
-		/* This event must be used only if controller is suspended */
-		if (hsotg->lx_state == DWC2_L2) {
-			dwc2_exit_hibernation(hsotg, true);
-			hsotg->lx_state = DWC2_L0;
-		}
-	}
-
-	if (gintsts & (GINTSTS_USBRST | GINTSTS_RESETDET)) {
-
-		u32 usb_status = readl(hsotg->regs + GOTGCTL);
-		u32 connected = hsotg->connected;
-
-		dev_dbg(hsotg->dev, "%s: USBRst\n", __func__);
-		dev_dbg(hsotg->dev, "GNPTXSTS=%08x\n",
-			readl(hsotg->regs + GNPTXSTS));
-
-		writel(GINTSTS_USBRST, hsotg->regs + GINTSTS);
-
-		/* Report disconnection if it is not already done. */
-		s3c_hsotg_disconnect(hsotg);
-
-		if (usb_status & GOTGCTL_BSESVLD && connected)
-			s3c_hsotg_core_init_disconnected(hsotg, true);
 	}
 
 	/* check both FIFOs */
