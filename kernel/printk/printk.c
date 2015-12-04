19e58f2854df8c1e206f50dbcf9b4b709bbbdf94
Liu Changcheng <changcheng.liu@intel.com>
2015-12-04 11:57:27 +0800

    printk not fully support call it in nmi handler for SMP arch.
    calling it in nmi handler for SMP arch.
    
    There is typical case in nmi handler function arch_trigger_all_cpu_backtrace_handler().
    
    In my platform, there are 2 CPUs, when function arch_trigger_all_cpu_backtrace()
    is called, 2 CPUs will recevied the nmi interrupts, and the
    arch_trigger_all_cpu_backtrace_handler() will called on 2 CPUs:
    
    case1:
    CPU0                                            CPU1
    calling arch_trigger_all_cpu_backtrace()        calling printk, and has obtain the logbuf_lock
    nmi interrupt received                          nmi interrupt received
    call arch_trigger_all_cpu_backtrace_handler()   call arch_trigger_all_cpu_backtrace_handler()
    Obtain arch_spin_lock(&lock);                   Waiting for arch_spin_lock(&lock);
    Continue to call printk()
    CPU0 will be blocked by logbuf_lock             CPU1 is blocked by arch_spin_lock(&lock)
    
    The deadlock will be happening.
    
    case2:
    CPU0                                             CPU1:(run dmesg command)
    calling arch_trigger_all_cpu_backtrace()         calling do_syslog
                                                     Obtaining the logbuf_lock
    nmi interrupt received                           nmi interrupt received
    ....
    The dealock will happen also somtimes.
    
    Change-Id: Id87fc0102143ec5c60dd7d7d0d27026c78a97707
    Orig-change-id: I8fcb579dfdb49b4f74a5269467d021b0498f0c05
    Original-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14095
    Signed-off-by: Chuansheng Liu <chuansheng.liu@intel.com>
    Reviewed-on: https://android.intel.com:443/453119
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-15324
    Reviewed-on: https://android.intel.com:443/456080

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1344,7 +1344,7 @@ static int console_trylock_for_printk(unsigned int cpu)
 {
 	int retval = 0, wake = 0;
 
-	if (console_trylock()) {
+	if (!in_nmi() && console_trylock()) {
 		retval = 1;
 
 		/*
@@ -1523,7 +1523,14 @@ asmlinkage int vprintk_emit(int facility, int level,
 	}
 
 	lockdep_off();
-	raw_spin_lock(&logbuf_lock);
+	if (unlikely(in_nmi())) {
+		if (!raw_spin_trylock(&logbuf_lock)) {
+			goto out_restore_lockdep_irqs;
+		}
+	} else {
+		raw_spin_lock(&logbuf_lock);
+	}
+
 	logbuf_cpu = this_cpu;
 
 	if (recursion_bug) {
@@ -1626,6 +1633,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 	if (console_trylock_for_printk(this_cpu))
 		console_unlock();
 
+out_restore_lockdep_irqs:
 	lockdep_on();
 out_restore_irqs:
 	local_irq_restore(flags);
