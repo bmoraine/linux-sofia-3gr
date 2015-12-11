703409b391a69492b12ac6db1a76cc9110c37573
Chuanxiao Dong <chuanxiao.dong@intel.com>
2015-12-11 15:06:53 +0800

    mmc: try sw reset if hw reset is not supported.
    Refine device reset process.
    
    Change-Id: I4e18c4016f6b3837ef9343b6b7d6293817e69c77
    Original-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-12835
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Yuan Juntao <juntao.yuan@intel.com>
    Reviewed-on: https://android.intel.com:443/452525
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-17463
    Reviewed-on: https://android.intel.com:443/462161

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -936,7 +936,7 @@ static int mmc_blk_reset(struct mmc_blk_data *md, struct mmc_host *host,
 		return -EEXIST;
 
 	md->reset_done |= type;
-	err = mmc_hw_reset(host);
+	err = mmc_do_reset(host);
 	/* Ensure we switch back to the correct partition */
 	if (err != -EOPNOTSUPP) {
 		struct mmc_blk_data *main_md = mmc_get_drvdata(host->card);
