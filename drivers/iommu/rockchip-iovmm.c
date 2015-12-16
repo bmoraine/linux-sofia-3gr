ff45c5d2f36031d30ac9608093828c0ae45325aa
Lei Zhang <lei.zhang@intel.com>
2015-12-16 16:27:05 +0800

    iommu: avoid list_del on one un-initialized list node
    Change-Id: I3eee9048f75ceccb12ea61d12336c9d7ea9177bd
    Original-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-12609
    Signed-off-by: Lei Zhang <lei.zhang@intel.com>
    Reviewed-on: https://android.intel.com:443/449324
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14933
    Reviewed-on: https://android.intel.com:443/454968

--- drivers/iommu/rockchip-iovmm.c
+++ drivers/iommu/rockchip-iovmm.c
@@ -146,17 +146,18 @@ dma_addr_t rockchip_iovmm_map(struct device *dev,
 
 	ret = rockchip_iommu_tlb_invalidate(dev);
 	if (ret)
-		goto err_map_map;
+		goto err_map_tlb_failure;
 
 	dev_dbg(dev->archdata.iommu, "IOVMM: Allocated VM region @ %#x/%#X bytes.\n",
 		 region->start, region->size);
 
 	return region->start;
 
-err_map_map:
+err_map_tlb_failure:
 	spin_lock(&vmm->lock);
 	list_del(&region->node);
 	spin_unlock(&vmm->lock);
+err_map_map:
 	iommu_unmap(vmm->domain, start, mapped_size);
 	gen_pool_free(vmm->vmm_pool, start, size);
 err_map_noiomem:
