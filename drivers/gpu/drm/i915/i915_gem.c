d3fc4666dbbf155f706be951455f2a3bad4f4c42
Mark Yao <mark.yao@rock-chips.com>
2016-02-25 12:50:25 +0800

    Introduce mapping_gfp_mask() instead of mapping_gfp_constraint()
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    Signed-off-by: B G, Pradeep <pradeep.b.g@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2275,8 +2275,9 @@ i915_gem_object_get_pages_gtt(struct drm_i915_gem_object *obj)
 	 * Fail silently without starting the shrinker
 	 */
 	mapping = file_inode(obj->base.filp)->i_mapping;
-	gfp = mapping_gfp_constraint(mapping, ~(__GFP_IO | __GFP_RECLAIM));
+	gfp = mapping_gfp_mask(mapping);
 	gfp |= __GFP_NORETRY | __GFP_NOWARN;
+	gfp &= ~(__GFP_IO | __GFP_RECLAIM);
 	sg = st->sgl;
 	st->nents = 0;
 	for (i = 0; i < page_count; i++) {
