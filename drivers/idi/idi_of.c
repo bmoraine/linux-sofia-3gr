12af8d83fd2a4ae8a0a073570c170b7c572976f4
Chang, Andrew <andrew.chang@intel.com>
2017-02-08 10:37:49 +0800

    drivers, idi: enable idi client interface to access BQ24296.
    Change-Id: Ia6bd859d6af1ceda47602ac7fae17ddee0442fdd
    Reviewed-by: Sit, Wei Hong <wei.hong.sit@intel.com>
    Signed-off-by: Chang, Andrew <andrew.chang@intel.com>
    Signed-off-by: Sia, Jee Heng <jee.heng.sia@intel.com>
    Reviewed-on: https://android.intel.com/567218
    Tested-by: Sit, Wei Hong <wei.hong.sit@intel.com>

--- drivers/idi/idi_of.c
+++ drivers/idi/idi_of.c
@@ -173,6 +173,8 @@ static struct idi_peripheral_type_lut type_lut[] = {
 	{.type = IDI_BAT, .property_value = "intel,idi,bat_hal"},
 	{.type = IDI_CHG, .property_value = "intel,idi,fan54x"},
 	{.type = IDI_CHG, .property_value = "intel,idi,smb345"},
+	{.type = IDI_CHG, .property_value = "intel,idi,bq24296"},
+	{.type = IDI_CHG, .property_value = "intel,idi,chgr"},
 	{.type = IDI_CCD, .property_value = "intel,idi,fg_hal"},
 	{.type = IDI_BNT, .property_value = "intel,idi,brownout"},
 	{.type = IDI_ERROR, .property_value = "intel,idi,error"},
